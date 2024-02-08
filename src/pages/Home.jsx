caca091404de83374f168b0f52148a025d2b69e2
Caleb Krysos <CalMc22>
2024-02-08 18:50:16 -0500

    added description to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -6,12 +7,18 @@ function Home() {
             <h1 className='title'>Home</h1>
             <ul>
                 <li>
-                    Hi, I'm Caleb McConnell!
+                    Welcome to Caleb McConnell's Portfolio!
                 </li>
                 <br />
                 <li>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam cumque quo iure consectetur nemo tenetur. Ad veritatis ex et, consequatur veniam, dicta quidem autem minima, a similique assumenda illum iure?
+                I am an aspiring Frontend Web Developer/Game Developer with a passion to create fun and user friendly experiences for anyone!
+                <br />
+                <br />
+                I'm thrilled to transition into the world of Junior Software Engineering. I have a background in woodworking, that has honed my attention to detail, precision, and problem-solving skills. I also have gained skills in HTML, CSS, Javascript, and REACT.js from Per Scholas. I feel that my background in both these fields allows me to offer a unique blend of skills and qualities that can benefit any team.
                 </li>
+                <br />
+                <br />
+                <li>Check out some <Link to="/projects">projects</Link> I worked on!</li>
             </ul>
         </div>
     );
